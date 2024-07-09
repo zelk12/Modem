a5e13c9a472dbec6c083052cd252aa7db189cf80
zelk12 <lenrchikz@mail.ru>
2024-07-09 16:10:32 +0400

    Первое сохранение.

---
+++ ModemMessageSystem.h
@@ -0,0 +1,22 @@
+/*
+ * ModemMessageSystem.h
+ *
+ *  Created on: 3 июл. 2024 г.
+ *      Author: TURTTON
+ */
+
+#ifndef INC_MODEMMESSAGESYSTEM_H_
+#define INC_MODEMMESSAGESYSTEM_H_
+
+#include "usart.h"
+#include "string.h"
+
+#include "Other.h"
+
+
+
+void Test();
+
+
+
+#endif /* INC_MODEMMESSAGESYSTEM_H_ */
