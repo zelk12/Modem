b162c69418839714d04bdf77255f2890853dddb7
zelk12 <lenrchikz@mail.ru>
2024-07-10 14:21:30 +0400

    Пересоздание. - Создана структура информации осервер - Создано перечисление протоколов подключения - Создано перечисление протоколов передачи

--- ModemMessageSystem.h
+++ ModemMessageSystem.h
@@ -11,12 +11,27 @@
 #include "usart.h"
 #include "string.h"
 
-#include "Other.h"
+#include "SoftTimerSystem.h"
 
+const typedef enum {
+	TCP, UDP,
+} protocols;
 
+const typedef enum{
+	GALILEOSKY,
+	EGTS_56360_2015,
+	EGTS_33472_2015,
+	GALYLEOSKY_COMPRESSED,
+} transferProtocols;
+
+typedef struct {
+	string mainServerAdres;
+	uint32_t port;
+	uint8_t protocol;
+	bool isUseTLSorSSL;
+	uint8_t transferProtocol;
+} serverData;
 
 void Test();
-
-
 
 #endif /* INC_MODEMMESSAGESYSTEM_H_ */
